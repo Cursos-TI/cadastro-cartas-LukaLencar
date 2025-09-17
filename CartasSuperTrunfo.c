36bce3d778ea90245140bc4e3f5e63211d69c8dd
LukaLencar <lualencar207@gmail.com>
2025-09-17 13:00:03 +0000

    carta jogador 1 pronta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main() {
   int populacao, numturistico;
   float areakm, pib;
   char estado, codigo[20], nome[100];
-
+//jogador n°1
   printf("Agora começaremos um jogo de Super Trunfo, jogador n°1, digite as informações de sua carta: \n");
   printf("Digite a opção de seu estado entre A até H: \n");
   scanf("%c", &estado);
@@ -34,12 +34,15 @@ int main() {
   printf("Digite o numero total de pontos turísticos: \n");
   scanf("%d", &numturistico);
 
-
+//carta jogador n°1
   printf("Carta Jogador n°1:\n");
   printf("Estado: %c\n", estado);
   printf("Codigo da Carta: %c%s\n", estado, codigo);
   printf("Nome da Cidade: %s\n", nome);
   printf("Quantidade Populacional: %d\n", populacao);
+  printf("Área: %f km²\n", areakm);
+  printf("PIB %f\n", pib);
+  printf("Número de Pontos Turísticos: %d\n", numturistico);
 
 return 0;
 } 
